a12427aca6159848cc255265b16d0200f78b387a
Aban_Dubey <131015588+Aban-Dubey@users.noreply.github.com>
2024-03-06 10:40:35 +0530

    remove double dash

--- server/server.js
+++ server/server.js
@@ -12,7 +12,9 @@ const app = express();
 // app.use(express.json());
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
 app.use(cors());
+
 app.use(morgan('tiny'));
 app.disable('x-powered-by'); //less hackers know about our stack
 app.use((err, req, res, next) => {
